e0ff6c9e9f4d31e4348e0f9f4accfb869178a277
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2017-05-30 04:15:35 +0200

    Add documentation for the location struct.

--- src/location.rs
+++ src/location.rs
@@ -1,3 +1,4 @@
+/// A pair of line and column representing a location within a file.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Location {
     pub line: u32,
@@ -6,6 +7,16 @@ pub struct Location {
 
 impl Location {
 
+    /// Construct a location representing the beginning of a file.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use charsloc::Location;
+    /// let location = Location::start();
+    /// assert_eq!(location.line, 1);
+    /// assert_eq!(location.column, 1);
+    /// ```
     #[inline]
     pub fn start() -> Location {
         Location {
@@ -14,12 +25,35 @@ impl Location {
         }
     }
 
+    /// Advances a location to the new line and resets the column.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use charsloc::Location;
+    /// let mut location = Location { line: 3, column: 5 };
+    /// location.next_line();
+    /// assert_eq!(location.line, 4);
+    /// assert_eq!(location.column, 1);
+    /// ```
     #[inline]
     pub fn next_line(&mut self) {
         self.column = 1;
         self.line += 1;
     }
 
+    /// Advances a location to the next column.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use charsloc::Location;
+    ///
+    /// let mut location = Location { line: 3, column: 5 };
+    /// location.next_column();
+    /// assert_eq!(location.line, 3);
+    /// assert_eq!(location.column, 6);
+    /// ```
     #[inline]
     pub fn next_column(&mut self) {
         self.column += 1;
