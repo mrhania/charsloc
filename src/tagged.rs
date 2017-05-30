16165aa00563ee92189cf915d02df6189579a0f9
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2017-05-30 06:18:33 +0200

    Add tests for the tagged iterator.

--- src/tagged.rs
+++ src/tagged.rs
@@ -49,3 +49,51 @@ impl<I: Iterator<Item=char>> Iterator for Tagged<I> {
         self.iter.size_hint()
     }
 }
+
+#[test]
+fn test_single_line() {
+    let mut iter = Tagged::new("foo".chars());
+    assert_eq!(iter.next(), Some(('f', Location { line: 1, column: 1 })));
+    assert_eq!(iter.next(), Some(('o', Location { line: 1, column: 2 })));
+    assert_eq!(iter.next(), Some(('o', Location { line: 1, column: 3 })));
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn test_multi_line() {
+    let mut iter = Tagged::new("a\nbc\nd".chars());
+    assert_eq!(iter.next(), Some(('a', Location { line: 1, column: 1 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 1, column: 2 })));
+    assert_eq!(iter.next(), Some(('b', Location { line: 2, column: 1 })));
+    assert_eq!(iter.next(), Some(('c', Location { line: 2, column: 2 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 2, column: 3 })));
+    assert_eq!(iter.next(), Some(('d', Location { line: 3, column: 1 })));
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn test_carriage_newline() {
+    let mut iter = Tagged::new("a\r\n\nb".chars());
+    assert_eq!(iter.next(), Some(('a', Location { line: 1, column: 1 })));
+    assert_eq!(iter.next(), Some(('\r', Location { line: 1, column: 2 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 1, column: 3 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 2, column: 1 })));
+    assert_eq!(iter.next(), Some(('b', Location { line: 3, column: 1 })));
+    assert_eq!(iter.next(), None);
+}
+
+#[test]
+fn test_dots() {
+    let mut iter = Tagged::new("\n\n.\n.\n\n..\n".chars());
+    assert_eq!(iter.next(), Some(('\n', Location { line: 1, column: 1 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 2, column: 1 })));
+    assert_eq!(iter.next(), Some(('.', Location { line: 3, column: 1 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 3, column: 2 })));
+    assert_eq!(iter.next(), Some(('.', Location { line: 4, column: 1 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 4, column: 2 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 5, column: 1 })));
+    assert_eq!(iter.next(), Some(('.', Location { line: 6, column: 1 })));
+    assert_eq!(iter.next(), Some(('.', Location { line: 6, column: 2 })));
+    assert_eq!(iter.next(), Some(('\n', Location { line: 6, column: 3 })));
+    assert_eq!(iter.next(), None);
+}
