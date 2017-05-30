503237194bbcf46576df1295f97b73e23abcd187
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2017-05-30 06:26:02 +0200

    Simplify test code of the tagged iterator.

--- src/tagged.rs
+++ src/tagged.rs
@@ -52,48 +52,53 @@ impl<I: Iterator<Item=char>> Iterator for Tagged<I> {
 
 #[test]
 fn test_single_line() {
-    let mut iter = Tagged::new("foo".chars());
-    assert_eq!(iter.next(), Some(('f', Location { line: 1, column: 1 })));
-    assert_eq!(iter.next(), Some(('o', Location { line: 1, column: 2 })));
-    assert_eq!(iter.next(), Some(('o', Location { line: 1, column: 3 })));
-    assert_eq!(iter.next(), None);
+    let tagged = Tagged::new("foo".chars()).collect::<Vec<_>>();
+    assert_eq!(tagged, vec!(
+        ('f', Location { line: 1, column: 1 }),
+        ('o', Location { line: 1, column: 2 }),
+        ('o', Location { line: 1, column: 3 }),
+    ));
 }
 
 #[test]
 fn test_multi_line() {
-    let mut iter = Tagged::new("a\nbc\nd".chars());
-    assert_eq!(iter.next(), Some(('a', Location { line: 1, column: 1 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 1, column: 2 })));
-    assert_eq!(iter.next(), Some(('b', Location { line: 2, column: 1 })));
-    assert_eq!(iter.next(), Some(('c', Location { line: 2, column: 2 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 2, column: 3 })));
-    assert_eq!(iter.next(), Some(('d', Location { line: 3, column: 1 })));
-    assert_eq!(iter.next(), None);
+    let tagged = Tagged::new("a\nbc\nd".chars()).collect::<Vec<_>>();
+    assert_eq!(tagged, vec!(
+        ('a', Location { line: 1, column: 1 }),
+        ('\n', Location { line: 1, column: 2 }),
+        ('b', Location { line: 2, column: 1 }),
+        ('c', Location { line: 2, column: 2 }),
+        ('\n', Location { line: 2, column: 3 }),
+        ('d', Location { line: 3, column: 1 }),
+    ));
 }
 
 #[test]
 fn test_carriage_newline() {
-    let mut iter = Tagged::new("a\r\n\nb".chars());
-    assert_eq!(iter.next(), Some(('a', Location { line: 1, column: 1 })));
-    assert_eq!(iter.next(), Some(('\r', Location { line: 1, column: 2 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 1, column: 3 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 2, column: 1 })));
-    assert_eq!(iter.next(), Some(('b', Location { line: 3, column: 1 })));
-    assert_eq!(iter.next(), None);
+    let tagged = Tagged::new("a\r\n\nb".chars()).collect::<Vec<_>>();
+    assert_eq!(tagged, vec!(
+        ('a', Location { line: 1, column: 1 }),
+        ('\r', Location { line: 1, column: 2 }),
+        ('\n', Location { line: 1, column: 3 }),
+        ('\n', Location { line: 2, column: 1 }),
+        ('b', Location { line: 3, column: 1 }),
+    ));
+    ;
 }
 
 #[test]
 fn test_dots() {
-    let mut iter = Tagged::new("\n\n.\n.\n\n..\n".chars());
-    assert_eq!(iter.next(), Some(('\n', Location { line: 1, column: 1 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 2, column: 1 })));
-    assert_eq!(iter.next(), Some(('.', Location { line: 3, column: 1 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 3, column: 2 })));
-    assert_eq!(iter.next(), Some(('.', Location { line: 4, column: 1 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 4, column: 2 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 5, column: 1 })));
-    assert_eq!(iter.next(), Some(('.', Location { line: 6, column: 1 })));
-    assert_eq!(iter.next(), Some(('.', Location { line: 6, column: 2 })));
-    assert_eq!(iter.next(), Some(('\n', Location { line: 6, column: 3 })));
-    assert_eq!(iter.next(), None);
+    let tagged = Tagged::new("\n\n.\n.\n\n..\n".chars()).collect::<Vec<_>>();
+    assert_eq!(tagged, vec!(
+        ('\n', Location { line: 1, column: 1 }),
+        ('\n', Location { line: 2, column: 1 }),
+        ('.', Location { line: 3, column: 1 }),
+        ('\n', Location { line: 3, column: 2 }),
+        ('.', Location { line: 4, column: 1 }),
+        ('\n', Location { line: 4, column: 2 }),
+        ('\n', Location { line: 5, column: 1 }),
+        ('.', Location { line: 6, column: 1 }),
+        ('.', Location { line: 6, column: 2 }),
+        ('\n', Location { line: 6, column: 3 }),
+    ));
 }
